ec18017701e744c43c8eee8c8002a6cf54e3fe07
morishin <sntr92@gmail.com>
2023-08-28 11:34:29 +0900

    Use VStack

--- src/app.tsx
+++ src/app.tsx
@@ -3,19 +3,21 @@ import preactLogo from "./assets/preact.svg";
 import viteLogo from "/vite.svg";
 import "./app.css";
 
+import { VStack } from "../styled-system/jsx";
+
 export function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
+      <VStack alignItems="center">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} class="logo" alt="Vite logo" />
         </a>
         <a href="https://preactjs.com" target="_blank">
           <img src={preactLogo} class="logo preact" alt="Preact logo" />
         </a>
-      </div>
+      </VStack>
       <h1>Vite + Preact</h1>
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
